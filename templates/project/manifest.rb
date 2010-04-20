653d3115e6d6d45aa133f7d218cfb5b73a3a469f
Eric Meyer <eric@oddbird.net>
2010-04-20 01:41:22 -0600

    converted to scss

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,3 +1,3 @@
 # Make sure you list all the project template files here in the manifest.
-stylesheet '_lightbox.sass', :media => 'screen, projection'
+stylesheet '_lightbox.scss', :media => 'screen, projection'
 html 'lightbox.html'
