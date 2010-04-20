5042778f27ae8a533d182e9b41ffc65cc6e3a849
Eric Meyer <eric@oddbird.net>
2010-04-20 01:46:42 -0600

    template metadata for better integration with compass command line

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,3 +1,16 @@
 # Make sure you list all the project template files here in the manifest.
 stylesheet '_lightbox.scss', :media => 'screen, projection'
 html 'lightbox.html'
+
+description "css-only lightboxes"
+
+help %Q{
+Installs some html and a stylesheet that you can use directly
+or refer to as an example.
+}
+
+welcome_message %Q{
+Please refer to the lightbox.html file to see how the markup should be structured.
+And to the lightbox stylesheet partial to see how to use the library and apply it to your markup.
+}
+
